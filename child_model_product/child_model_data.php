de884ce174e8301a3d34548781e6d94d099f0368
omprakash <omprakashyadav5257@gmail.com>
2022-10-13 18:09:02 +0530

    new child

--- child_model_product/child_model_data.php
+++ child_model_product/child_model_data.php
@@ -2,61 +2,98 @@
 $productFileData=[];
 $modelData=[];
 $childSkuData=[];
-$allProductFile=fopen(__DIR__.'/product_data.csv',"r");
-$modelProductFile=fopen("C:/xampp/htdocs/relight/modelproduct/newModel.csv","r");
+$allProductFile=fopen("C:/xampp/htdocs/relight/csvupload/Product.csv","r");
+$modelProductFile=fopen("C:/xampp/htdocs/relight/modelproduct/model_product.csv","r");
 while (($productData = fgetcsv($allProductFile)) !== FALSE) {
-    $productFileData[] = $productData; 
+  $productFileData[] = $productData; 
 }
 fclose($allProductFile);
-
 $x=0;
 while (($modelProductData = fgetcsv($modelProductFile)) !== FALSE) {
     if($x != 0) {
+        $parentSku= $modelProductData[2];
         $childSku= $modelProductData[3];
         $productId=$modelProductData[0];
         if($productId == 0) {
         } else {
-           $modelData[] =array($childSku);
+           $modelData[] =array(
+              'child_sku' => $childSku,
+              'parent_sku' =>$parentSku
+            );
+          
         }
     }
      $x++;
 }
 fclose($modelProductFile);
-
+$a=0;
 foreach($modelData as $childData) {
-    $arr=explode(",",$childData[0]);
+    $arr=array(
+      "data" => explode(",",$childData['child_sku'])
+    );
+    
+    $additional = array($a++ =>$childData['parent_sku']);
+    $arr['data'] = array_merge($arr['data'], $additional);
     array_push($childSkuData,$arr);
 }
-
 $newArrayData=[];
 foreach($childSkuData as $childArrayData) {
   foreach($childArrayData as $data) {
     $newArrayData[]=$data;
   }
 }
-
-
-$newData=(array_chunk($newArrayData,1));
 $productCsvData=[];
-$count=count($newData);
-for($i=0;$i<$count;$i++) {
-  $j=0;
-  foreach($productFileData  as $product) {
-    if($newData[$i][0] ==$product[4]) {
-       $productCsvData[]=$product;
+
+foreach($newArrayData as $skuData) {
+  $parentData=array_pop($skuData);
+  for($k=0;$k < count($skuData);$k++) {
+    $j=0;
+    foreach($productFileData as $product) {
+     if($skuData[$k]==$product[4]) {
+       $productCsvData[]=array(
+        'product' => $product,
+        'parent_sku' => $parentData,
+        'child_sku' => $skuData[$k], 
+       );
+     }
+     $j++;
     }
-    $j++;
   }
 }
 
+$result=[];
 $newArraypushed=[];
-foreach($productCsvData as $arrayData) {
-  $array=array(
-  'data' =>explode('=',$arrayData[2]),
-  'price' => $arrayData[10]); 
-  echo "<pre>";
-  print_r($array);
+foreach($productCsvData as $arrayData) { 
+   $colorTemp= (explode('=',$arrayData['product'][2]));
+  if($colorTemp[0] == '') {
+  } else {
+    $result[$colorTemp[0]] = $colorTemp[1];
+    $array=array(
+      'parent_sku' =>$arrayData['parent_sku'],
+      'child_sku' => $arrayData['child_sku'],
+      'price' => $arrayData['product'][10],
+      'cost_price' =>$arrayData['product'][11],
+      'retail_price' =>$arrayData['product'][12],
+      'sale_price' => $arrayData['product'][13],
+  );
+  $mergeData=array_merge($result,$array);
+  array_push($newArraypushed,$mergeData);
+  }
+}
+$fileName='child-model-Product'.date("d-m-y").'-'.time().'.csv';
+$downloadDir= __DIR__."/csvFile/".$fileName;
+$fileOpen = fopen($downloadDir, "w");
+
+$csvFileHeader[]=array('sku','parent',"Color_Temperature",'price','cost_price','Retail_price','sale_price');
+foreach($newArraypushed as $csvData) {
+ $color=(preg_split("/\,/", $csvData['[S]Color Temperature (CCT)'])); 
+  $colorArray=($color[0]);
+  $colorNewArray=preg_split("/\s+/",$colorArray);
+ $csvFileHeader[]=array($csvData['child_sku'],$csvData['parent_sku'],$colorNewArray[0],$csvData['price'],$csvData['cost_price'],$csvData['retail_price'],$csvData['sale_price']);
 }
 
+foreach($csvFileHeader as $csvValue) {
+  fputcsv($fileOpen,$csvValue);
+}
 
 ?>
--- new_data_com.php
+++ new_data_com.php
@@ -24,12 +24,13 @@ while (false !== ($entry = $directory->read())) {
         $latestFilename = $entry;
     }
 }
+
 $newArrayData=[];
 $row=0;
 $productSku='';
 $productSku=[];
 $additional=[];
-$fileOpen=fopen(__DIR__."/newcsvfile/".$latestFilename,"r");
+$fileOpen=fopen(__DIR__."/newcsvfile/".$latestFilename,"r"); //product file.
 while (($data = fgetcsv($fileOpen)) !== FALSE) {
     if($row== 0) {
         $row++; 
@@ -71,45 +72,48 @@ foreach($newArrayData as $customValue) {
             $newArray[$key2] =str_replace('"', "",$value2);
         }
     }
-
+    
    array_push($allData,$newArray);
  
 }
 
+
 $filtedCsvData[]=array('Product_sku' ,'Light_Source','Fixture_Size','Listings and Ratings','Ballast and Voltage','Color Rendering Index (CRI)','Driver Type','Lumen Output','L70 Expected Life (hours)','L90 Expected Life (hours)','Color Temperature','Total Input Watt','Total Input Watts','Pack Size','Luminaire Efficacy Rating (LER)','Color Temperature (CCT)','Max Ambient Temp','Lamp Count','Lens','Socket','Application','Insulation Rating','__alsobought','Rated Life','Max Ambient');
 
-$fileName='new-simple-Product-'.date("d-m-y").'-'.time().'.csv';
-$downloadDir= __DIR__."/newproduct/".$fileName;
-$fileOpen = fopen($downloadDir, "w");
-foreach($allData as $csvData) {
-    @$product_sku=$csvData['"product_sku'];
-    @$light_source=$csvData['"Light Source'];
-    @$fixture_size=$csvData['"Fixture Size'];
-    @$lightRating=$csvData['"Listings and Ratings'];
-    @$lightBallest=$csvData['"Ballast and Voltage'];
-    @$colorRender=$csvData['"Color Rendering Index (CRI)'] ;
-    @$driver_type=$csvData['"Driver Type'];
-    @$lumenOutput=$csvData['"Lumen Output'];
-    @$expectedLife=$csvData['"L70 Expected Life (hours)'];
-    @$l_expectedlif=$csvData['"L90 Expected Life (hours)'];
-    @$colorTemp=$csvData['"Color Temperature'];
-    @$inputWatt=$csvData['"Total Input Watt'];
-    @$totalWatts=$csvData['"Total Input Watts'];
-    @$packSize=$csvData['"Pack Size'];
-    @$efficiancy=$csvData['"Luminaire Efficacy Rating (LER)'];
-    @$colorTempCct=$csvData['"Color Temperature (CCT)'];
-    @$maxEmbientTemp=$csvData['"Max Ambient Temp'];
-    @$lampCount=$csvData['"Lamp Count'];
-    @$lense=$csvData['Lens'];
-    @$socket=$csvData['Socket'];
-    @$application = $csvData['Application'];
-    @$insulationRating = $csvData['"Insulation Rating'];
-    @$alsobought = $csvData['__alsobought'];
-    @$ratedLife = $csvData['"Rated Life'];
-    @$maxAmbient = $csvData['"Max Ambient'];
-    @$filtedCsvData[]=array(@$product_sku,@$light_source,@$fixture_size,@$lightRating, @$lightBallest,@$colorRender,@$driver_type,@$lumenOutput,@$expectedLife,@$l_expectedlif,@$colorTemp,@$inputWatt,@$totalWatts,@$packSize,@$efficiancy,@$colorTempCct,@$maxEmbientTemp,@$lampCount,@$lense,@$socket,@$application,@$insulationRating,@$alsobought,@$ratedLife,@$maxAmbient);
-}
-foreach($filtedCsvData as $newData){
-    fputcsv($fileOpen,$newData);
-}
+// $fileName='new-simple-Product-'.date("d-m-y").'-'.time().'.csv';
+// $downloadDir= __DIR__."/newproduct/".$fileName;
+// $fileOpen = fopen($downloadDir, "w");
+
+// foreach($allData as $csvData) {
+//     @$product_sku=$csvData['"product_sku'];
+//     @$light_source=$csvData['"Light Source'];
+//     @$fixture_size=$csvData['"Fixture Size'];
+//     @$lightRating=$csvData['"Listings and Ratings'];
+//     @$lightBallest=$csvData['"Ballast and Voltage'];
+//     @$colorRender=$csvData['"Color Rendering Index (CRI)'] ;
+//     @$driver_type=$csvData['"Driver Type'];
+//     @$lumenOutput=$csvData['"Lumen Output'];
+//     @$expectedLife=$csvData['"L70 Expected Life (hours)'];
+//     @$l_expectedlif=$csvData['"L90 Expected Life (hours)'];
+//     @$colorTemp=$csvData['"Color Temperature'];
+//     @$inputWatt=$csvData['"Total Input Watt'];
+//     @$totalWatts=$csvData['"Total Input Watts'];
+//     @$packSize=$csvData['"Pack Size'];
+//     @$efficiancy=$csvData['"Luminaire Efficacy Rating (LER)'];
+//     @$colorTempCct=$csvData['"Color Temperature (CCT)'];
+//     @$maxEmbientTemp=$csvData['"Max Ambient Temp'];
+//     @$lampCount=$csvData['"Lamp Count'];
+//     @$lense=$csvData['Lens'];
+//     @$socket=$csvData['Socket'];
+//     @$application = $csvData['Application'];
+//     @$insulationRating = $csvData['"Insulation Rating'];
+//     @$alsobought = $csvData['__alsobought'];
+//     @$ratedLife = $csvData['"Rated Life'];
+//     @$maxAmbient = $csvData['"Max Ambient'];
+//     @$filtedCsvData[]=array(@$product_sku,@$light_source,@$fixture_size,@$lightRating, @$lightBallest,@$colorRender,@$driver_type,@$lumenOutput,@$expectedLife,@$l_expectedlif,@$colorTemp,@$inputWatt,@$totalWatts,@$packSize,@$efficiancy,@$colorTempCct,@$maxEmbientTemp,@$lampCount,@$lense,@$socket,@$application,@$insulationRating,@$alsobought,@$ratedLife,@$maxAmbient);
+// }
+// foreach($filtedCsvData as $newData){
+//     fputcsv($fileOpen,$newData);
+// }
+
 ?>
